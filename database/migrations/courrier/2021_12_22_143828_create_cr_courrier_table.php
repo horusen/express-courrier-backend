ff5ce834d82290e6f330a044fbcfb0f63481d399
satexgado <satexgado@gmail.com>
2021-12-22 16:56:48 +0000

    feat (migration): add courrier

---
+++ database/migrations/courrier/2021_12_22_143828_create_cr_courrier_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrCourrierTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cr_courrier', function (Blueprint $table) {
+            $table->id();
+            $table->string('libelle');
+            $table->string('objet');
+            $table->date('date_redaction');
+            $table->text('commentaire')->nullable();
+
+            // Debut Champ(nature): facture
+            $table->string('numero_facture')->nullable();
+            $table->string('devise')->nullable();
+            $table->int('montant')->nullable();
+            // Fin Champ(nature): facture
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreignId('type')->constrained('cr_type')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('nature')->constrained('cr_nature')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('structure')->constrained('structures')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('suivi_par')->constrained('inscription')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('inscription')->constrained('inscription')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cr_courrier');
+    }
+}
---
+++ database/migrations/courrier/2021_12_22_151547_create_cr_courrier_interne_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrCourrierInterneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cr_courrier_interne', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreignId('courrier')->constrained('cr_courrier')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('inscription')->constrained('inscription')->onDelete('restrict')->onUpdate('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cr_courrier_interne');
+    }
+}
