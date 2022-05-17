ca9ad8d65a04ad5920e200ad4f94322e1400d0c4
satexgado <satexgado@gmail.com>
2022-05-17 11:54:06 +0000

    quick

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::get('/storage', function () {
+    Artisan::call('storage:link');
+});
